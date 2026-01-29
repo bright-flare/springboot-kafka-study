40cc183abd28a11c05a2bb19e21fef66ec50c6a7
심현섭_백오피스서비스개발팀 <hyunseob.shim@oliveyoung.co.kr>
2026-01-29 12:59:33 +0900

    feat: h2 db 붙여서 테스트해보자 ~

---
+++ src/main/java/com/example/user/User.java
@@ -0,0 +1,31 @@
+﻿package com.example.user;
+
+import jakarta.persistence.*;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+public class User {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  private String email;
+
+  private String name;
+
+  private String password;
+
+  public User(String password, String name, String email) {
+    this.password = password;
+    this.name = name;
+    this.email = email;
+  }
+  
+  public static User of(String password, String name, String email) {
+    return new User(password, name, email);
+  }
+  
+}
